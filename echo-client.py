d0563cecf05dc42de496cb45088a7d2f7310ce16
VladParaschiv <vladparaschiv03@gmail.com>
2020-11-26 22:18:20 +0200

    modificat client si server si modificat documentatia

--- echo-client.py
+++ echo-client.py
@@ -1,15 +1,33 @@
 #!/usr/bin/env python3
 
 import socket
+import logging
 
-HOST = '127.0.0.1'	# localhost
-PORT = 65432		# port (non--privileged port are > 1023 ) 0-65536
-MESSAGE = b"Hello, Iuli!"	# message to send
-
+HOST = '127.0.0.1'		# localhost
+PORT = 65432			# port (non--privileged port are > 1023 ) 0-65536
+	
 # create a new socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# socket.sendto(bytes, address)
-	# send data to socket
-s.sendto(MESSAGE, (HOST,PORT))
-		
+server_address = (HOST,PORT)
+
+logging.basicConfig(filename="Client.log", format='%(asctime)s %(message)s', filemode='w', datefmt='%m/%d/%Y %I:%M:%S')
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+while True:
+	try:
+		send_data = input ("Client send: ")
+		logger.info(send_data)
+		s.sendto(send_data.encode('utf-8'), server_address)
+
+
+		data, addr = s.recvfrom(1024)
+		print ("Message received on server: ", data.decode('utf-8'), "\n")
+	except KeyboardInterrupt:
+		print("\nClient went off")
+		break
+
+s.close()
+	
--- echo-server.py
+++ echo-server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import socket
+import logging
 
 HOST = '127.0.0.1'	# localhost
 PORT = 65432		# port (non-privileged ports are > 1023) 0-65536
@@ -11,10 +12,27 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # socket.bind(address)
 	#bind the socket to address
-s.bind((HOST,PORT))
+server_address = (HOST,PORT)
+s.bind(server_address)
+
+logging.basicConfig(filename="Server.log", format='%(asctime)s %(message)s', filemode='w', datefmt='%m/%d/%Y %I:%M:%S')
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 while True:
+	try:
 	# socket.recvfrom(bufsize [,flags])
 		# receive data from the socket. The return value is a pair (bytes, address) where bytes is a btes object representing the data received and address is the address of the socket sending the data.
-	data, addr = s.recvfrom(1)
-	print("received message: %s" %data)
+		data, addr = s.recvfrom(1024)
+		print("Message received from: ", addr)
+		print("Server received: ", data.decode('utf-8'), "\n")
+
+		logger.info(data.decode('utf-8'))
+
+		send_data=data.decode('utf-8')
+		s.sendto(send_data.encode('utf-8'),addr)
+	except KeyboardInterrupt:
+		print("\nServer went off")
+		break
+
